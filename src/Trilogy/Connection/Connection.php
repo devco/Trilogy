4bf1f7fa4b323bbe947e5b942818669818fdd365
Trey Shugart <treshugart@gmail.com>
2012-09-12 15:43:11 +1000

    Update default config.

--- src/Trilogy/Connection/Connection.php
+++ src/Trilogy/Connection/Connection.php
@@ -23,6 +23,7 @@ class Connection implements ConnectionInterface
     public static $defaults = [
         'driver'   => 'mysql',
         'host'     => 'localhost',
+        'port'     => null,
         'database' => 'default',
         'username' => 'root',
         'password' => '',
