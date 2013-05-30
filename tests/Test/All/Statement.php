4b9051580bc30320c3f8720647a2af6d654608e9
Russell Holman <rholman@ultraserve.com.au>
2013-05-30 11:10:24 +1000

    Fixed Test Was trying to retrieve parameters from the compiled query instead of the statement.

--- tests/Test/All/Statement.php
+++ tests/Test/All/Statement.php
@@ -48,10 +48,10 @@ class Statement extends UnitAbstract
     public function findJoinAndOnValue()
     {
         $x = 9;
-        $find = $this->db->find->in('a')->where('a.a = ?', 1)->join('b')->on('b.a = a.b')->andOn('b.c', $x)->compile();
+        $find = $this->db->find->in('a')->where('a.a = ?', 1)->join('b')->on('b.a = a.b')->andOn('b.c', $x);
         $comp = 'SELECT * FROM "a" INNER JOIN "b" ON "b"."a" = "a"."b" AND "b"."c" = ? WHERE "a"."a" = ?';
 
-        $this->assert($find === $comp, 'Compilation failed.');
+        $this->assert($find->compile() === $comp, 'Compilation failed.');
 
         $params = $find->params();
 
